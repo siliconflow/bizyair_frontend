847729e5206c0bfd6413c69bd541529ae11c13ca
linjm8780860 <lin8780860@163.com>
2024-12-13 11:14:57 +0800

    fix

---
+++ src/components/modules/vImage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+const aspectRatio = ref<null | boolean>(null)
+const props = defineProps({
+    src: String,
+    alt: String,
+})
+watch(() => props.src, () => {
+    const img = new Image()
+    img.src = props.src as string
+    img.onload = () => {
+        aspectRatio.value = img.width > img.height
+    }
+}, { immediate: true })
+</script>
+
+<template>
+    <div :class="`w-full h-full relative ${src? 'bg-black': ''} overflow-hidden image-box`">
+        <!-- <div class="absolute bg-slate-600 w-full h-full">
+            <span class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">loadding</span>
+        </div> -->
+        <img 
+            :src="src" 
+            :alt="alt" 
+            :class="`absolute ${aspectRatio ? 'w-[100%] h-auto' : 'w-auto h-[100%]'} left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all hover:scale-125`" />
+    </div>
+</template>
--- src/components/modules/vUploadImage.vue
+++ src/components/modules/vUploadImage.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import vImage from '@/components/modules/vImage.vue'
+import { Trash2 } from 'lucide-vue-next';
 
 const imageSrc = ref('')
 
@@ -47,8 +49,9 @@ const handleFileChange = (e: Event) => {
   <div class="w-32 h-32 border border-dashed border-slate-300 rounded-lg relative overflow-hidden">
     <input class="cursor-pointer opacity-0 w-full h-full absolute left-0 top-0 z-20" type="file"
       @change="handleFileChange" />
-    <img :src="imageSrc" class="w-full h-full object-cover rounded-lg absolute left-0 top-0 z-10" />
+    <vImage :src="imageSrc" class="block object-cover w-32 h-32 rounded-lg absolute left-0 top-0 z-10" />
     <div class="w-20 h-1 absolute left-6 top-1/2 -translate-y-1/2 bg-slate-400 rounded-md"></div>
     <div class="w-1 h-20 absolute top-6 left-1/2 -translate-x-1/2 bg-slate-400 rounded-md"></div>
+    <Trash2 class="absolute right-2 top-2 z-30 cursor-pointer" v-if="imageSrc" @click="imageSrc = ''" />
   </div>
 </template>
