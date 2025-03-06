8318758c1be05d966156b6883d71d4546aece78a
of-HaiQin <5372604@qq.com>
2025-03-06 14:56:54 +0800

    [feat]fix auto load template workflow json

--- src/components/community/Mine.vue
+++ src/components/community/Mine.vue
@@ -51,7 +51,15 @@
 
     if (workflow.data && comfyUIApp && comfyUIApp.graph) {
       comfyUIApp.graph.clear()
-      await comfyUIApp.loadGraphData(workflow.data)
+      if(workflow.data.templates && workflow.data.templates.length > 0){
+          await comfyUIApp.loadTemplateData(workflow.data)
+      }
+      else{
+          await comfyUIApp.loadGraphData(workflow.data)
+      }
+    }
+    else{
+        useToaster.error('Failed to load workflow')
     }
     communityStore.showDialog = false
   }
