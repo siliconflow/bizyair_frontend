4447b22902f83f0f652c067bbec245e02a90f15e
林峻明 <lin8780860@163.com>
2025-03-20 11:28:01 +0800

    修改个人中心、添加微信支付 (#32)
    * fix
    
    * 添加支付功能
    
    * auto prettier format code
    
    ---------
    
    Co-authored-by: linjm8780860 <11494038+linjm8780860@users.noreply.github.com>

--- src/components/modules/vUpload/vUploadImage.vue
+++ src/components/modules/vUpload/vUploadImage.vue
@@ -60,10 +60,14 @@
       />
       <vImage
         :src="imageSrc"
-        class="block object-cover w-32 h-32 rounded-lg absolute left-0 top-0 z-10"
+        class="block object-cover w-full h-full rounded-lg absolute left-0 top-0 z-10"
       />
-      <div class="w-20 h-1 absolute left-6 top-1/2 -translate-y-1/2 bg-slate-400 rounded-md"></div>
-      <div class="w-1 h-20 absolute top-6 left-1/2 -translate-x-1/2 bg-slate-400 rounded-md"></div>
+      <div
+        class="w-[64%] h-1 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-400 rounded-md"
+      ></div>
+      <div
+        class="w-1 h-[64%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-400 rounded-md"
+      ></div>
       <Trash2
         class="absolute right-2 top-2 z-30 cursor-pointer"
         v-if="imageSrc"
---
+++ src/views/btnProfile/uploadAvatar.vue
@@ -0,0 +1,81 @@
+<template>
+  <n-modal
+    v-model:show="statusStore.showUploadAvatarDialog"
+    preset="card"
+    style="width: 580px"
+    title="avatar"
+    :auto-focus="false"
+    :mask-closable="false"
+    :bordered="false"
+  >
+    <ul class="avatar-list">
+      <li v-for="(e, i) in avatar" :key="i">
+        <vImage class="pic" :src="e" alt="" @click="selectThis(e)" />
+      </li>
+      <!-- <li>
+        <vUploadImage
+          className="w-24 h-24"
+          v-model.modelValue="cover_url"
+          @done="imageUploadDone"
+        />
+      </li> -->
+    </ul>
+  </n-modal>
+</template>
+<script setup lang="ts">
+  import { NModal } from 'naive-ui'
+  import { useStatusStore } from '@/stores/userStatus'
+  import { ref } from 'vue'
+  import vImage from '@/components/modules/vImage.vue'
+  // import vUploadImage from '@/components/modules/vUpload/vUploadImage.vue'
+
+  const statusStore = useStatusStore()
+  const avatar = ref([
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/ViKdyI5vrD7XGNCXHuVTW4sPUXNusj3W.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/YDP6ZVq41yj9HCn4oQklNps7gkYXmYtF.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/am4o3LW21fiCtkHBn6sVcJQCabyXPRpP.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/SoEIUUVIlRI4jmS0oJGksF2oYWTXiFRH.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/neWI6jKICQYZmhGjrr4EEQ3ayyP9mTZr.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/5YPQStbsKrGqkZzwGvkHXzGhy2XX5bcb.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/yXPbljOblCjqRTWgbcHIhV6Qmtbo3Q3M.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/6GjafFtlRKO0Tb7j4pQFSUHBsRinI8Fl.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/qcVtbtFeBtk1bejEQ2lik4uUfvF6grKK.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/BRuIn69IDL74l0K5qqi4HA3GEqLSS209.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/NXywfJUeDimurtmtVmVM8PrkuFwyafoN.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/8JHxWscVaxw3at3uzhKqb2HR6V1O2YGr.webp',
+    'https://bizyair-prod.oss-cn-shanghai.aliyuncs.com/web/McHuaZdFjCoqUf57m4j5XVanuDDRJTo1.webp'
+  ])
+  // const cover_url = ref('')
+  const selectThis = (e: string) => {
+    statusStore.usersMetadata.avatar = e
+    statusStore.showUploadAvatarDialog = false
+  }
+  // const imageUploadDone = () => {
+  //   statusStore.usersMetadata.avatar = cover_url.value
+  //   statusStore.showUploadAvatarDialog = false
+  // }
+</script>
+<style scoped lang="less">
+  .avatar-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    li {
+      margin: 0;
+      padding: 0;
+      width: 100px;
+      height: 100px;
+      .pic {
+        width: 100px;
+        cursor: pointer;
+      }
+    }
+    .upload-image {
+      width: 100px;
+      height: 100px;
+    }
+  }
+</style>
