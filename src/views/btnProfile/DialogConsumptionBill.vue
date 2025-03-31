4301abe55297bbe18d0dcde0a1565e78e0a4deb4
林峻明 <lin8780860@163.com>
2025-03-31 18:49:49 +0800

    修改样式 (#41)
    * fix
    
    * auto prettier format code
    
    ---------
    
    Co-authored-by: linjm8780860 <11494038+linjm8780860@users.noreply.github.com>

--- src/views/btnProfile/DialogConsumptionBill.vue
+++ src/views/btnProfile/DialogConsumptionBill.vue
@@ -270,6 +270,36 @@
           }
         )
       }
+    },
+    {
+      title: ' ',
+      key: 'handle',
+      render() {
+        return h(
+          NTooltip,
+          { trigger: 'hover', placement: 'top' },
+          {
+            default: () => t('btnProfile.consumptionBill.monthlyBill'),
+            trigger: () => {
+              return h(
+                'svg',
+                {
+                  xmlns: 'http://www.w3.org/2000/svg',
+                  width: '16',
+                  height: '16',
+                  viewBox: '0 0 1024 1024'
+                },
+                [
+                  h('path', {
+                    fill: '#fff',
+                    d: 'M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1l-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4'
+                  })
+                ]
+              )
+            }
+          }
+        )
+      }
     }
   ]
 
@@ -338,6 +368,36 @@
           {
             default: () => `${row.gift_amount || 0}`,
             trigger: () => formatCoinAmount(row.gift_amount || 0)
+          }
+        )
+      }
+    },
+    {
+      title: ' ',
+      key: 'handle',
+      render() {
+        return h(
+          NTooltip,
+          { trigger: 'hover', placement: 'top' },
+          {
+            default: () => t('btnProfile.consumptionBill.dailyBill2'),
+            trigger: () => {
+              return h(
+                'svg',
+                {
+                  xmlns: 'http://www.w3.org/2000/svg',
+                  width: '16',
+                  height: '16',
+                  viewBox: '0 0 1024 1024'
+                },
+                [
+                  h('path', {
+                    fill: '#fff',
+                    d: 'M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1l-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4'
+                  })
+                ]
+              )
+            }
           }
         )
       }
