869fff83c11976387c77cdab144854122ba58c6b
lin8780860@163.com <lin8780860@163.com>
2025-07-04 20:52:14 +0800

    fix-apiKey

--- src/views/btnApiKey/apiKeyDialog.vue
+++ src/views/btnApiKey/apiKeyDialog.vue
@@ -96,8 +96,8 @@
     const authUrl = `${ACCOUNT_ENDPOINT}/oauth?client_id=${clientId}`
     const popup = window.open(authUrl, 'oauthPopup', 'width=600,height=600')
     window.addEventListener('message', event => {
-      if (event.data.length > 0 && event.data[0]['secretKey'] !== undefined) {
-        setKey(event.data[0]['secretKey'])
+      if (event.data.data.length > 0 && event.data.data[0]['secretKey'] !== undefined) {
+        setKey(event.data.data[0]['secretKey'])
         if (popup) {
           popup.close()
         }
