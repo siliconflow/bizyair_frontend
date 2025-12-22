2693c7ae26e95a4f82bc9f416880d8e950b8f74e
topfunplusnew <ming10655@outlook.com>
2025-12-22 11:48:24 +0100

    给所有bizyair三方节点添加定价展示 (#154)

--- bizyui/js/handle_node_configure.js
+++ bizyui/js/handle_node_configure.js
@@ -3,13 +3,12 @@ import { applyBadgeToNode } from "./handle_load_nodes.js";
 app.registerExtension({
   name: "bizyair.handle.node.configure",
   nodeCreated(node, app) {
-    // 简单的节点配置处理逻辑
-    console.log("Node created:", node);
+    const possibleWidgetNames = ["model_name", "model"];
     // 在这里可以拿到变化之后的值，并且也可以拿到node，这时候给node切换badge即可
     if (node && node.widgets && Array.isArray(node.widgets)) {
       // 只有model选择的widget才注册callback函数
       node.widgets.forEach((widget) => {
-        if (widget.name === "model_name") {
+        if (possibleWidgetNames.includes(widget.name)) {
           widget.callback = async function () {
             await applyBadgeToNode(node);
           };
