f4bd23b90cd473f1cef0e3ee89d9c60ec483ddb0
topfunplusnew <ming10655@outlook.com>
2025-12-24 15:32:14 +0800

    修复节点信息切换不加载价格问题

--- bizyui/js/handle_node_configure.js
+++ bizyui/js/handle_node_configure.js
@@ -3,16 +3,20 @@ import { applyBadgeToNode } from "./handle_load_nodes.js";
 app.registerExtension({
   name: "bizyair.handle.node.configure",
   nodeCreated(node, app) {
-    const possibleWidgetNames = ["model_name", "model"];
+    // const possibleWidgetNames = ["model_name", "model"];
     // 在这里可以拿到变化之后的值，并且也可以拿到node，这时候给node切换badge即可
     if (node && node.widgets && Array.isArray(node.widgets)) {
       // 只有model选择的widget才注册callback函数
+      // 不仅仅是切换model才会修改模型定价，比如切换输入参数也会修改模型定价
       node.widgets.forEach((widget) => {
-        if (possibleWidgetNames.includes(widget.name)) {
-          widget.callback = async function () {
+        widget.callback = async function () {
             await applyBadgeToNode(node);
           };
-        }
+        // if (possibleWidgetNames.includes(widget.name)) {
+        // widget.callback = async function () {
+        //   await applyBadgeToNode(node);
+        // };
+        // }
       });
     }
   },
