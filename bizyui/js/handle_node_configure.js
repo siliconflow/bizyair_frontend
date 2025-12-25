413764ecbbaa09300d382b8ab8dedf2b3535cfbf
topfunplusnew <ming10655@outlook.com>
2025-12-25 11:49:08 +0100

    修复某些节点请求参数错误，修复被误伤的comfyui原生的上传widget等 (#159)
    * 修复节点问题
    
    * 修改某些节点价格展示问题

--- bizyui/js/handle_node_configure.js
+++ bizyui/js/handle_node_configure.js
@@ -1,18 +1,20 @@
 import { app } from "../../scripts/app.js";
 import { applyBadgeToNode } from "./handle_load_nodes.js";
+import { hasModelInput } from "./model_price.js";
 app.registerExtension({
   name: "bizyair.handle.node.configure",
   nodeCreated(node, app) {
-    // const possibleWidgetNames = ["model_name", "model"];
     // 在这里可以拿到变化之后的值，并且也可以拿到node，这时候给node切换badge即可
     if (node && node.widgets && Array.isArray(node.widgets)) {
-      // 只有model选择的widget才注册callback函数
+      if (!hasModelInput(node)) {
+        return;
+      }
       // 不仅仅是切换model才会修改模型定价，比如切换输入参数也会修改模型定价
       node.widgets.forEach((widget) => {
-        // widget.callback = async function () {
-        //   // 用户手动修改widget时，强制刷新badge（不使用缓存的模型类型）
-        //   await applyBadgeToNode(node, true);
-        // };
+        widget.callback = async function () {
+          // 用户手动修改widget时，强制刷新badge（不使用缓存的模型类型）
+          await applyBadgeToNode(node, true);
+        };
       });
     }
   },
